81140c8ce698e6036ba3773dde88054e5018494a
zablockijj <zablockijj@gmail.com>
2014-06-03 09:47:04 -0400

    Bootstrap added

---
+++ app/views/hello.blade - Copy.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="description" content="">
+	<meta name="author" content="ink, cookbook, recipes">
+	<meta name="HandheldFriendly" content="True">
+	<meta name="MobileOptimized" content="320">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<title>ReadNuts</title>
+	{{ HTML::style('css/ink.css'); }}
+	{{ HTML::style('css/ink-flex.css'); }}
+	{{ HTML::style('css/font-awesome.css'); }}
+	{{ HTML::script('js/modernizr.js'); }}
+	<script type="text/javascript">
+		Modernizr.load({
+		  test: Modernizr.flexbox,
+		  nope : 'http://cdn.ink.sapo.pt/3.0.0/css/ink-legacy.min.css'
+		});
+	</script>
+	{{ HTML::script('js/holder.js'); }}
+	{{ HTML::script('js/ink-all.min.js'); }}
+	{{ HTML::script('js/autoload.js'); }}
+	<style>
+		body {
+			background: #ededed;
+		}
+		.panel {
+			border-radius: 2px;
+			-webkit-box-shadow: #dddddd 0 1px 1px 0;
+			-moz-box-shadow: #dddddd 0 1px 1px 0;
+			box-shadow: #dddddd 0 1px 1px 0;
+			padding: 1em;
+			border: 1px solid #BBB;
+			background-color: #FFF;
+		}
+	</style>
+</head>
+<body>
+	<div class="ink-grid vertical-space">
+		<!--[if lte IE 9 ]>
+		<div class="ink-alert basic" role="alert">
+			<button class="ink-dismiss">&times;</button>
+			<p>
+				<strong>You are using an outdated Internet Explorer version.</strong>
+				Please <a href="http://browsehappy.com/">upgrade to a modern browser</a> to improve your web experience.
+			</p>
+		</div>
+		-->
+
+		<div class="panel">
+
+			<div id="car1" class="ink-carousel" data-space-after-last-slide="false">
+
+				<ul class="stage column-group half-gutters">
+
+					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
+						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/1">
+						<div class="description">
+							<h4 class="no-margin">Highlight Title</h4>
+							<h5 class="slab">Baby Doe</h5>
+							<p>É importante questionar o quanto a constante divulgação das informações ainda não demonstrou convincentemente que vai participar na mudança do impacto na agilidade decisória.</p>
+						</div>
+					</li>
+
+					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
+						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/2">
+						<div class="description">
+							<h4 class="no-margin">Highlight Title</h4>
+							<h5 class="slab">Baby Doe</h5>
+							<p>Devemos cultivar os nossos soft powers sagrados</p>
+						</div>
+					</li>
+
+					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
+						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/3">
+						<div class="description">
+							<h4 class="no-margin">Highlight Title</h4>
+							<h5 class="slab">Baby Doe</h5>
+							<p>Rumo ao inconseguimento de cenas</p>
+						</div>
+					</li>
+
+					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
+						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/4">
+						<div class="description">
+							<h4 class="no-margin">Highlight Title</h4>
+							<h5 class="slab">Baby Doe</h5>
+							<p>É importante questionar o quanto a constante divulgação das informações ainda não demonstrou convincentemente que vai participar na mudança do impacto na agilidade decisória.</p>
+						</div>
+					</li>
+
+					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
+						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/5">
+						<div class="description">
+							<h4 class="no-margin">Highlight Title</h4>
+							<h5 class="slab">Baby Doe</h5>
+							<p>É importante questionar o quanto a constante divulgação das informações ainda não demonstrou convincentemente que vai participar na mudança do impacto na agilidade decisória.</p>
+						</div>
+					</li>
+
+					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
+						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/6">
+						<div class="description">
+							<h4 class="no-margin">Highlight Title</h4>
+							<h5 class="slab">Baby Doe</h5>
+							<p>É importante questionar o quanto a constante divulgação das informações ainda não demonstrou convincentemente que vai participar na mudança do impacto na agilidade decisória.</p>
+						</div>
+					</li>
+
+					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
+						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/7">
+						<div class="description">
+							<h4 class="no-margin">Highlight Title</h4>
+							<h5 class="slab">Baby Doe</h5>
+							<p>É importante questionar o quanto a constante divulgação das informações ainda não demonstrou convincentemente que vai participar na mudança do impacto na agilidade decisória.</p>
+						</div>
+					</li>
+
+					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
+						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/8">
+						<div class="description">
+							<h4 class="no-margin">Highlight Title</h4>
+							<h5 class="slab">Baby Doe</h5>
+							<p>É importante questionar o quanto a constante divulgação das informações ainda não demonstrou convincentemente que vai participar na mudança do impacto na agilidade decisória.</p>
+						</div>
+					</li>
+
+					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
+						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/9">
+						<div class="description">
+							<h4 class="no-margin">Highlight Title</h4>
+							<h5 class="slab">Baby Doe</h5>
+							<p>É importante questionar o quanto a constante divulgação das informações ainda não demonstrou convincentemente que vai participar na mudança do impacto na agilidade decisória.</p>
+						</div>
+					</li>
+				</ul>
+
+			</div>
+
+			<nav id="p1" class="ink-navigation" data-instance="pagination1">
+				<ul class="pagination black">
+				</ul>
+			</nav>
+
+		</div>
+
+		<script>
+			Ink.requireModules(['Ink.UI.Carousel_1'], function(InkCarousel) {
+				new InkCarousel('#car1', {pagination: '#p1'});
+			});
+		</script>
+	</div>
+
+</body>
+</html>
--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -1,157 +1,68 @@
-<!doctype html>
-<html lang="en">
-<head>
+<!DOCTYPE html>
+<html lang="en"><head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="">
-	<meta name="author" content="ink, cookbook, recipes">
-	<meta name="HandheldFriendly" content="True">
-	<meta name="MobileOptimized" content="320">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-	<title>ReadNuts</title>
-	{{ HTML::style('css/ink.css'); }}
-	{{ HTML::style('css/ink-flex.css'); }}
+	<meta name="author" content="">
+	<title>Starter Template for Bootstrap</title>
+	
+	{{ HTML::style('css/bootstrap.css'); }}
 	{{ HTML::style('css/font-awesome.css'); }}
-	{{ HTML::script('js/modernizr.js'); }}
-	<script type="text/javascript">
-		Modernizr.load({
-		  test: Modernizr.flexbox,
-		  nope : 'http://cdn.ink.sapo.pt/3.0.0/css/ink-legacy.min.css'
-		});
-	</script>
-	{{ HTML::script('js/holder.js'); }}
-	{{ HTML::script('js/ink-all.min.js'); }}
-	{{ HTML::script('js/autoload.js'); }}
-	<style>
-		body {
-			background: #ededed;
-		}
-		.panel {
-			border-radius: 2px;
-			-webkit-box-shadow: #dddddd 0 1px 1px 0;
-			-moz-box-shadow: #dddddd 0 1px 1px 0;
-			box-shadow: #dddddd 0 1px 1px 0;
-			padding: 1em;
-			border: 1px solid #BBB;
-			background-color: #FFF;
-		}
-	</style>
-</head>
-<body>
-	<div class="ink-grid vertical-space">
-		<!--[if lte IE 9 ]>
-		<div class="ink-alert basic" role="alert">
-			<button class="ink-dismiss">&times;</button>
-			<p>
-				<strong>You are using an outdated Internet Explorer version.</strong>
-				Please <a href="http://browsehappy.com/">upgrade to a modern browser</a> to improve your web experience.
-			</p>
-		</div>
-		-->
+	{{ HTML::style('css/main.css'); }}
+	<link href="starter-template.css" rel="stylesheet">
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<style id="clearly_highlighting_css" type="text/css">/* selection */ html.clearly_highlighting_enabled ::-moz-selection { background: rgba(246, 238, 150, 0.99); } html.clearly_highlighting_enabled ::selection { background: rgba(246, 238, 150, 0.99); } /* cursor */ html.clearly_highlighting_enabled {    /* cursor and hot-spot position -- requires a default cursor, after the URL one */    cursor: url("chrome-extension://pioclpoplcdbaefihamjohnefbikjilc/clearly/images/highlight--cursor.png") 14 16, text; } /* highlight tag */ em.clearly_highlight_element {    font-style: inherit !important; font-weight: inherit !important;    background-image: url("chrome-extension://pioclpoplcdbaefihamjohnefbikjilc/clearly/images/highlight--yellow.png");    background-repeat: repeat-x; background-position: top left; background-size: 100% 100%; } /* the delete-buttons are positioned relative to this */ em.clearly_highlight_element.clearly_highlight_first { position: relative; } /* delete buttons */ em.clearly_highlight_element a.clearly_highlight_delete_element {    display: none; cursor: pointer;    padding: 0; margin: 0; line-height: 0;    position: absolute; width: 34px; height: 34px; left: -17px; top: -17px;    background-image: url("chrome-extension://pioclpoplcdbaefihamjohnefbikjilc/clearly/images/highlight--delete-sprite.png"); background-repeat: no-repeat; background-position: 0px 0px; } em.clearly_highlight_element a.clearly_highlight_delete_element:hover { background-position: -34px 0px; } /* retina */ @media (min--moz-device-pixel-ratio: 2), (-webkit-min-device-pixel-ratio: 2), (min-device-pixel-ratio: 2) {    em.clearly_highlight_element { background-image: url("chrome-extension://pioclpoplcdbaefihamjohnefbikjilc/clearly/images/highlight--yellow@2x.png"); }    em.clearly_highlight_element a.clearly_highlight_delete_element { background-image: url("chrome-extension://pioclpoplcdbaefihamjohnefbikjilc/clearly/images/highlight--delete-sprite@2x.png"); background-size: 68px 34px; } } </style><style>[touch-action="none"]{ -ms-touch-action: none; touch-action: none; }[touch-action="pan-x"]{ -ms-touch-action: pan-x; touch-action: pan-x; }[touch-action="pan-y"]{ -ms-touch-action: pan-y; touch-action: pan-y; }[touch-action="scroll"],[touch-action="pan-x pan-y"],[touch-action="pan-y pan-x"]{ -ms-touch-action: pan-x pan-y; touch-action: pan-x pan-y; }</style>
+	</head>
+	<body>
+		<nav>
+			<ul class="list-unstyled main-menu">
+				<!--Include your navigation here-->
+				<li class="text-right"><a href="#" id="nav-close">X</a></li>
+				<li><a href="#">Menu One <span class="icon"></span></a></li>
+				<li><a href="#">Menu Two <span class="icon"></span></a></li>
+				<li><a href="#">Menu Three <span class="icon"></span></a></li>
+				<li><a href="#">Dropdown<span class="caret"></span></a></li>
+				<li><a href="#">Menu Four <span class="icon"></span></a></li>
+				<li><a href="#">Menu Five <span class="icon"></span></a></li>
+			</ul>
+		</nav>
 
-		<div class="panel">
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
 
-			<div id="car1" class="ink-carousel" data-space-after-last-slide="false">
+    <div class="container">
 
-				<ul class="stage column-group half-gutters">
+      <div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      </div>
 
-					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
-						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/1">
-						<div class="description">
-							<h4 class="no-margin">Highlight Title</h4>
-							<h5 class="slab">Baby Doe</h5>
-							<p>É importante questionar o quanto a constante divulgação das informações ainda não demonstrou convincentemente que vai participar na mudança do impacto na agilidade decisória.</p>
-						</div>
-					</li>
-
-					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
-						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/2">
-						<div class="description">
-							<h4 class="no-margin">Highlight Title</h4>
-							<h5 class="slab">Baby Doe</h5>
-							<p>Devemos cultivar os nossos soft powers sagrados</p>
-						</div>
-					</li>
-
-					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
-						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/3">
-						<div class="description">
-							<h4 class="no-margin">Highlight Title</h4>
-							<h5 class="slab">Baby Doe</h5>
-							<p>Rumo ao inconseguimento de cenas</p>
-						</div>
-					</li>
-
-					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
-						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/4">
-						<div class="description">
-							<h4 class="no-margin">Highlight Title</h4>
-							<h5 class="slab">Baby Doe</h5>
-							<p>É importante questionar o quanto a constante divulgação das informações ainda não demonstrou convincentemente que vai participar na mudança do impacto na agilidade decisória.</p>
-						</div>
-					</li>
-
-					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
-						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/5">
-						<div class="description">
-							<h4 class="no-margin">Highlight Title</h4>
-							<h5 class="slab">Baby Doe</h5>
-							<p>É importante questionar o quanto a constante divulgação das informações ainda não demonstrou convincentemente que vai participar na mudança do impacto na agilidade decisória.</p>
-						</div>
-					</li>
-
-					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
-						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/6">
-						<div class="description">
-							<h4 class="no-margin">Highlight Title</h4>
-							<h5 class="slab">Baby Doe</h5>
-							<p>É importante questionar o quanto a constante divulgação das informações ainda não demonstrou convincentemente que vai participar na mudança do impacto na agilidade decisória.</p>
-						</div>
-					</li>
-
-					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
-						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/7">
-						<div class="description">
-							<h4 class="no-margin">Highlight Title</h4>
-							<h5 class="slab">Baby Doe</h5>
-							<p>É importante questionar o quanto a constante divulgação das informações ainda não demonstrou convincentemente que vai participar na mudança do impacto na agilidade decisória.</p>
-						</div>
-					</li>
-
-					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
-						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/8">
-						<div class="description">
-							<h4 class="no-margin">Highlight Title</h4>
-							<h5 class="slab">Baby Doe</h5>
-							<p>É importante questionar o quanto a constante divulgação das informações ainda não demonstrou convincentemente que vai participar na mudança do impacto na agilidade decisória.</p>
-						</div>
-					</li>
-
-					<li class="slide xlarge-25 large-25 medium-33 small-100 tiny-100">
-						<img class="half-bottom-space" src="http://lorempixel.com/400/200/sports/9">
-						<div class="description">
-							<h4 class="no-margin">Highlight Title</h4>
-							<h5 class="slab">Baby Doe</h5>
-							<p>É importante questionar o quanto a constante divulgação das informações ainda não demonstrou convincentemente que vai participar na mudança do impacto na agilidade decisória.</p>
-						</div>
-					</li>
-				</ul>
-
-			</div>
-
-			<nav id="p1" class="ink-navigation" data-instance="pagination1">
-				<ul class="pagination black">
-				</ul>
-			</nav>
-
-		</div>
-
-		<script>
-			Ink.requireModules(['Ink.UI.Carousel_1'], function(InkCarousel) {
-				new InkCarousel('#car1', {pagination: '#p1'});
-			});
-		</script>
-	</div>
-
+    </div><!-- /.container -->
+	
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	{{ HTML::script('js/bootstrap.js'); }}
 </body>
 </html>
